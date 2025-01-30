a1d597c30937e88d2e86dfe463f45791cf86e5c1
Garren Ramirez <gramirez4466@sdsu.edu>
2025-01-29 20:38:24 -0800

    Final Commit

--- skeleton.py
+++ skeleton.py
@@ -1,3 +1,5 @@
+from operator import index
+
 class CrewMember:
 
     def __init__(self, name, role, experience):
@@ -15,14 +17,18 @@ class CrewRoster:
 
     def add_member(self, name, role, experience):
         """Creates a new crew member and adds to roster."""
-
+        self.crew.append(CrewMember(name, role, experience))
 
     def remove_member(self, name):
         """Removes a crew member by name."""
-
+        for member in self.crew:
+            if member.name == name:
+                self.crew.remove(member)
 
     def list_crew(self):
         """Prints all crew members."""
+        for i in range (len(self.crew)):
+            print(self.crew[i].__str__())
 
 
 # === TEST CODE ===
@@ -30,11 +36,8 @@ class CrewRoster:
 roster=CrewRoster() #Empty Crew roster created
 
     # TODO: Uncomment and implement methods
-    # roster.add_member("Alice", "Engineer", 5)
-    # roster.add_member("Bob", "Pilot", 10)
-    # roster.list_crew()
-
-    # roster.remove_member("Alice")
-    # roster.list_crew()
-
-print("Cheeseburger:D")
+roster.add_member("Alice", "Engineer", 5)
+roster.add_member("Bob", "Pilot", 10)
+roster.list_crew()
+roster.remove_member("Alice")
+roster.list_crew()
