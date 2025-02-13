5b342d1d488d1788f9c3cc7004e329a5dba55ad6
Garren Ramirez <gramirez4466@sdsu.edu>
2025-02-13 11:29:45 -0800

    Test Commit

---
+++ Stack.py
@@ -0,0 +1,18 @@
+class Stack:
+    def __init__(self):
+        """Initialize an empty stack."""
+
+    def push(self, item):
+        """Push an item onto the stack."""
+
+    def pop(self):
+        """Remove and return the top item from the stack. Raise an error if empty."""
+
+    def peek(self):
+        """Return the top item without removing it. Raise an error if empty."""
+
+    def is_empty(self):
+        """Return True if the stack is empty, False otherwise."""
+
+    def size(self):
+        """Return the number of items in the stack."""
